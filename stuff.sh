eeca257098f1fcbf56d34ca4df88a61c0595f848
Adam Farley <adfarley@redhat.com>
2021-05-13 14:46:15 +0100

    abc

---
+++ stuff.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo rogerroger;
+cd /home/runner/work/build-jdk/build-jdk/openjdk-build/workspace/./build//src;
+bash ./configure --verbose --with-boot-jdk=/home/runner/work/build-jdk/build-jdk/jdk/boot --enable-ccache --with-jvm-variants=server --disable-ccache --disable-warnings-as-errors --enable-dtrace;
+echo endofcommand;
+
+exit 0;
