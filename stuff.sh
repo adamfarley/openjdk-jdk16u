270352ab8d0407a7c6e837010fa9340da9736158
Adam Farley <adfarley@redhat.com>
2021-05-13 15:28:58 +0100

    rrr

--- stuff.sh
+++ stuff.sh
@@ -2,6 +2,9 @@
 
 echo rogerroger;
 cd /home/runner/work/build-jdk/build-jdk/openjdk-build/workspace/./build//src;
+whoami
+sudo su
+whoami
 echo "sudo bash ./configure --verbose --with-boot-jdk=/home/runner/work/build-jdk/build-jdk/jdk/boot --enable-ccache --with-jvm-variants=server --disable-ccache --disable-warnings-as-errors --enable-dtrace;"
 sudo bash ./configure --verbose --with-boot-jdk=/home/runner/work/build-jdk/build-jdk/jdk/boot --enable-ccache --with-jvm-variants=server --disable-ccache --disable-warnings-as-errors --enable-dtrace;
 echo endofcommand;
